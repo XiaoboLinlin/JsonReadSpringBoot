6d9f9c294ae1bc24e413e04fb6d5c88aee49802a
XiaoboLinlin <xiaobolin11@outlook.com>
2023-12-17 17:06:41 -0600

    delete some useless

--- src/main/java/com/json_springboot_read/JsonSpringbootReadApplication.java
+++ src/main/java/com/json_springboot_read/JsonSpringbootReadApplication.java
@@ -22,39 +22,4 @@ public class JsonSpringbootReadApplication {
         SpringApplication.run(JsonSpringbootReadApplication.class, args);
         System.out.println("hello");
     }
-//
-//    @Bean
-//    CommandLineRunner runner(UserCombService userCombService) {
-//        return args -> {
-//            ObjectMapper mapper = new ObjectMapper();
-//            List<UserComb> userCombs = new ArrayList<>();
-//
-//            try (InputStream inputStream1 = TypeReference.class.getResourceAsStream("/Json/file1.json");
-//                 InputStream inputStream2 = TypeReference.class.getResourceAsStream("/Json/file2.json")) {
-//
-//                // Read the files
-//                TypeReference<List<User>> typeRefUser = new TypeReference<>() {
-//                };
-//                List<User> users = mapper.readValue(inputStream1, typeRefUser);
-//
-//                TypeReference<List<userAppend>> typeRefUserAppend = new TypeReference<>() {
-//                };
-//                List<userAppend> usersAppend = mapper.readValue(inputStream2, typeRefUserAppend);
-//
-//                // Combine data
-//                for (int i = 0; i < users.size(); i++) {
-//                    User user = users.get(i);
-//                    userAppend userappend = usersAppend.get(i * 10); // Ensure that this does not cause IndexOutOfBoundsException
-//                    UserComb userComb = new UserComb(user, userappend); // Assuming such a constructor exists
-//                    userCombs.add(userComb);
-//                }
-//                userCombService.setUserCombs(userCombs);
-//
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//
-//            // Use the combined list 'userCombs' as needed
-//        };
-//    }
 }
